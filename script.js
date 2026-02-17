0c183482dd7117ffe1e3051674f41560416c6f55
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-02-17 14:47:30 -0500

    Add logic to make the spans disappear after 3 seconds.

--- script.js
+++ script.js
@@ -8,4 +8,10 @@ bodyEl.addEventListener("mousemove", (event) => {
   spanEl.style.left = xPos + "px";
   spanEl.style.top = yPos + "px";
   bodyEl.appendChild(spanEl);
+
+  // After 3 seconds we want to remove the span.
+  // This will make the hearts disappear after 3 seconds.
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
 });
